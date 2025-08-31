db3071cde52f7253c632be60105f44725d4f2102
Brayden <bcunningham11@huskers.unl.edu>
2025-08-31 11:46:53 -0500

    added my name and major

--- hello.c
+++ hello.c
@@ -6,7 +6,11 @@ int main(int argc,char **argv){
     printf("My name is Daniel Beckman\n");
 
     printf("My Major is Robotics Engineering\n");
-    
+
+    printf("My name is Brayden Cunningham\n");
+
+    printf("My Major is Computer Engineering\n");
+
     return 0;
 
 }
